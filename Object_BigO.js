09edaa76b9a40a874087f260c41cfd46de392604
dalebert <38624799+dalebert@users.noreply.github.com>
2023-04-03 09:51:18 -0400

    Create Object_BigO.js

---
+++ Object_BigO.js
@@ -0,0 +1,18 @@
+let instructor = {
+    firstName = "Kelly",
+    isInstructor = true,
+    favoriteNumbers: [1,2,3,4]
+}
+
+// Big O of Objects //
+
+// Insertion - O(1)
+// Removal - O(1)
+// Access - O(1)
+// Searching - O(n)
+
+// Big O of Object Methods //
+
+// Object.keys(instructor) - O(n)
+// Object.entries(instructor) - O(n)
+// Object.hasOwnProperty("firstName") - O(1)
