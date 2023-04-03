c9f6e30f129e47692241e951c561232876223c04
dalebert <38624799+dalebert@users.noreply.github.com>
2023-04-03 09:54:33 -0400

    Create Arrays_BigO.js
    All comments -- just notes on calculating BigO when arrays are involved. This is mostly review but nice to have.

---
+++ Arrays_BigO.js
@@ -0,0 +1,13 @@
+// Use arrays when order matters
+// Complcates insertion and removal sometimes
+
+// Big O of arrays
+// Accessing - O(1)
+// Insertion depends on order
+    // Add to end (push) is fast - O(1)
+    // Add to beginning is slow because requires re-indexing
+// Removing is similar
+    // Removing from end (pop) is fast - O(1)
+    // Removing from beginning also requires re-indexing
+// Searching unordered - O(n)
+// Searching ordered - O(logN))
